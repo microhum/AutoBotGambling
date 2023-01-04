713d10057073969a59811d8f9e24391048a12a43
microhum <116241698+microhum@users.noreply.github.com>
2023-01-04 20:57:47 +0700

    'deleted some license updated some ui'

--- mainfile.py
+++ mainfile.py
@@ -4,32 +4,52 @@ import time
 import random
 import cooldowns as cd
 import pynput
+import os
 from pynput.keyboard import Key, Controller
 import config
 keyboard = Controller()
 pmtypes = ["f", "r", "i", "c", "k"]
-
+workcount = 0
+slutcount = 0
+crimecount = 0
+depcount = 0
+def clear():
+    os.system( 'cls' )
 
 def type_send(msg):
-    print("sent",msg)
     keyboard.type(str(msg))
     time.sleep(2)
     keyboard.press(Key.enter)
+    clear()
+    print("--History--")
+    print("work :",workcount)
+    print("slut :",slutcount)
+    print("crime :",crimecount)
+    print("dep :",depcount)
+    
 
 def print_work(scheduler):
     type_send(";work")
+    global workcount
+    workcount += 1
     scheduler.run_after(print_work, cd.work_cooldown)
 
 def print_slut(scheduler):
     type_send(";slut")
+    global slutcount
+    slutcount += 1
     scheduler.run_after(print_slut, cd.slut_cooldown)
 
 def print_crime(scheduler):
     type_send(";crime")
+    global crimecount
+    crimecount += 1
     scheduler.run_after(print_crime, cd.crime_cooldown)
 
 def print_dep(scheduler):
     type_send(";dep all")
+    global depcount
+    depcount += 1
     scheduler.run_after(print_dep, cd.dep_cooldown)
 
 class Scheduler:
